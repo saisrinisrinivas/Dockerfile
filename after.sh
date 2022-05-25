b6e6266a79a8e95c57028d581a6e30953a482171
saisrinisrinivas <102722606+saisrinisrinivas@users.noreply.github.com>
2022-05-25 12:04:51 +0530

    Update after.sh

--- after.sh
+++ after.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker rm nginx -f
-docker run --name nginx  -d -p 80:80 -it 874504233743.dkr.ecr.us-east-1.amazonaws.com/hellopipeline
+docker run --name nginx  -d -p 80:80 -it 874504233743.dkr.ecr.us-east-1.amazonaws.com/helloworld
 echo "Pipeline constructed well"
